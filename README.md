283d98b1fbecfb0192cb81ef2042d0b9ce9416b4
Rik <r.dedeken@itude.com>
2019-08-23 13:56:04 +0200

    more hacks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # brizy
 scrape aqualim for station details
+
+The techniques for scraping dynamic websites were inspired by https://towardsdatascience.com/data-science-skills-web-scraping-javascript-using-python-97a29738353f
--- try_stations_details_aqualim.py
+++ try_stations_details_aqualim.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 import brizy
 
-df = brizy.get_stations_www()
+driver = brizy.start_driver()
+
+df = brizy.get_stations_www(driver)
 print(df)
+
+station_details = brizy.retrieveMesure(driver, 'L6510')
+print(station_details)
